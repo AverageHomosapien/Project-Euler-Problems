1d6a90ad0df1f294bf84406a0cd82f4e91718fa0
hamcam98 <hamcalum10@gmail.com>
2019-07-07 21:45:01 +0100

    Problem 7 Complete

---
+++ Problem7.py
@@ -0,0 +1,35 @@
+# What is the 10 001st prime number?
+# Date Last Modified: 07/07/2019
+# Author: Calum Hamilton
+
+
+def main():
+    counter = 4
+    primes = 2
+    found = False
+    prime_check = True
+    while found == False:
+        for x in range(2,counter):
+            if counter % x == 0:
+                prime_check = False
+                break
+
+        if prime_check == True:
+            primes += 1
+
+        if primes >= 10001:
+            found = True
+        else:
+            counter +=1
+            prime_check = True
+
+    print("%d is the answer for number 7!" %(counter))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
